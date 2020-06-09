9ad80e2b939d8ae68375c78d7ddce16dfe327508
yz <oliver.yang@tophant.com>
2020-06-09 23:15:31 +0800

    update article, :tada:

---
+++ update.sh
@@ -0,0 +1,9 @@
+# /bin/bash
+cd my_website
+rm -rf public
+hugo  --theme=loveIt --baseUrl="https://yzthewind.github.io" --buildDrafts
+cd ..
+cp -r my_website/public/* ./
+git add .
+git commit -m "update article, :tada:"
+git push
